220e0ed443088e7fb47b7bc6912a4e69f3fe91dc
NukLaochaem <71333106+NukLaochaem@users.noreply.github.com>
2021-03-18 09:12:34 +0100

    feedback

--- Js/script.js
+++ Js/script.js
@@ -1,17 +1,54 @@
-const addToBag = document.querySelector (".ADDTOBAG_BTN");
+const addToBag = document.querySelector (".addToBag_itemPage");
 const succesMsg = document.querySelector (".succes_msg")
 
-addToBag.addEventListener("click", message);
 
+// 'add to bag' pop up and close Item page
+addToBag.addEventListener("click", message);
 function message(event){
     succesMsg.style.display = "block";
+    
 }
 
-
-const cross = document.querySelector (".cross");
-
-cross.addEventListener("click", closeMsg);
+const cross_cart = document.querySelector (".cross_cart");
+cross_cart.addEventListener("click", closeMsg);
 
 function closeMsg(event){
     succesMsg.style.display = "none";
+}
+
+
+
+// 'add to favorite' pop up and close Item page
+
+const favorite = document.querySelector (".item_heart")
+const favoriteMsg = document.querySelector (".favorite_msg")
+const cross_favorite = document.querySelector (".cross_favorite");
+
+favorite.addEventListener("click", favoriteMeassge)
+function favoriteMeassge(event){
+    favoriteMsg.style.display = "block";
+}
+
+
+cross_favorite.addEventListener("click", closeFavorite);
+function closeFavorite(event){
+    favoriteMsg.style.display = "none";
+}
+
+// 'add to bag' on favorite page
+
+const popUp = document.querySelector (".pop_up")
+const addToBagFavorite = document.querySelector (".addtobag_btn")
+const closingPopUp = document.querySelector (".close_popup_cross")
+
+addToBagFavorite.addEventListener("click", showPopUp);
+
+function showPopUp(event){
+    popUp.style.display = "block"
+}
+
+closingPopUp.addEventListener("click", closePopUp);
+
+function closePopUp(event){
+    popUp.style.display = "none";
 }
