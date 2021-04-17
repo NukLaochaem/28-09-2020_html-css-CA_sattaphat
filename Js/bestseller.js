405d4867056f7543b841dbdbf3f75b355fa014e1
NukLaochaem <71333106+NukLaochaem@users.noreply.github.com>
2021-04-17 10:10:18 +0200

    img

--- Js/bestseller.js
+++ Js/bestseller.js
@@ -2,8 +2,6 @@ const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const getJacketID = params.get("id")
 
-
-
 const IdUrl = "http://nuklaochaem.one/rainydays/wordpress-5.7/wordpress/wp-json/wc/store/products/"+ getJacketID;
 const corsFix = "https://noroffcors.herokuapp.com/";
 const jacketUrl = corsFix + IdUrl;
@@ -16,19 +14,17 @@ const itemPrice = document.querySelector(".red_rain_jacket_price")
 const itemDetails = document.querySelector(".item_details_container")
 
 
-
-console.log(itemDetails);
 async function callApi(){
     try{
         const response = await fetch(jacketUrl)
         const bestSellerItem = await response.json(response)
         console.log(bestSellerItem);
         
-        modelItem.innerHTML += `${bestSellerItem.images[0].src}`;
+        modelItem.innerHTML += `<img src="${bestSellerItem.images[0].src}" alt="">`
+
         itemName.innerHTML += `${bestSellerItem.name}`;
         itemPrice.innerHTML += `${bestSellerItem.prices.price} $`;
         itemDetails.innerHTML += `${bestSellerItem.description}`;
-
             
     } catch (error){
         console.log(error);
